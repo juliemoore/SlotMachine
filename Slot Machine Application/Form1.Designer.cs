5298d048d654fa8740d07d4d27d9c878179c9eac
Julie Moore <msjulieannmoore@gmail.com>
2018-05-09 20:41:39 -0500

    First commit

---
+++ Slot Machine Application/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Slot_Machine_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.moneyTextBox = new System.Windows.Forms.TextBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Slot_Machine_Application.Properties.Resources.Watermelon;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Slot_Machine_Application.Properties.Resources.Orange;
+            this.pictureBox2.Location = new System.Drawing.Point(149, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Slot_Machine_Application.Properties.Resources.Pear;
+            this.pictureBox3.Location = new System.Drawing.Point(284, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 128);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount Inserted: $";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // moneyTextBox
+            // 
+            this.moneyTextBox.Location = new System.Drawing.Point(218, 166);
+            this.moneyTextBox.Name = "moneyTextBox";
+            this.moneyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.moneyTextBox.TabIndex = 4;
+            // 
+            // spinButton
+            // 
+            this.spinButton.Location = new System.Drawing.Point(128, 207);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(75, 23);
+            this.spinButton.TabIndex = 5;
+            this.spinButton.Text = "Spin";
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(222, 207);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Watermelon.bmp");
+            this.imageList1.Images.SetKeyName(1, "Orange.bmp");
+            this.imageList1.Images.SetKeyName(2, "Pear.bmp");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "Watermelon.bmp");
+            this.imageList2.Images.SetKeyName(1, "Orange.bmp");
+            this.imageList2.Images.SetKeyName(2, "Pear.bmp");
+            // 
+            // imageList3
+            // 
+            this.imageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList3.ImageStream")));
+            this.imageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList3.Images.SetKeyName(0, "Watermelon.bmp");
+            this.imageList3.Images.SetKeyName(1, "Orange.bmp");
+            this.imageList3.Images.SetKeyName(2, "Pear.bmp");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 241);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.moneyTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox moneyTextBox;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+    }
+}
+
---
+++ Slot Machine Application/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_Machine_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        decimal winTotal = 0m;      // Total winning accumulator
+        decimal moneyTotal = 0m;    // Total money spent accumulator
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            // Variables
+            decimal money;
+            decimal winning;
+            
+
+            // Create Random objects
+            Random rand = new Random();
+
+            // Get random indexes
+            int index1 = rand.Next(imageList1.Images.Count);
+
+            // Display image1
+            pictureBox1.Image = imageList1.Images[index1];
+
+            // Get another random number
+            int index2 = rand.Next(imageList2.Images.Count);
+
+            // Display image2
+            pictureBox2.Image = imageList2.Images[index2];
+
+            // Get another random number
+            int index3 = rand.Next(imageList3.Images.Count);
+
+            // Display image3
+            pictureBox3.Image = imageList3.Images[index3];
+            
+            if (decimal.TryParse(moneyTextBox.Text, out money))
+            {
+                // Add the money to the total
+                moneyTotal += money;
+
+                if (index1 == index2 && index1 == index3 && index2 == index3)
+                {
+                    // Calculate winning
+                    winning = money * 3;
+
+                    // Add the winnings to the total
+                    winTotal += winning;
+
+                    // Display winnings
+                    MessageBox.Show("You won " + winning.ToString("c"));
+                }
+
+                else if (index1 == index2 || index1 == index3 || index2 == index3)
+                {
+                    // Calculate winning
+                    winning = money * 2;
+
+                    // Add the winnings to the total
+                    winTotal += winning;
+
+                    // Display winnings
+                    MessageBox.Show("You won " + winning.ToString("c"));
+                }
+
+                else if (index1 != index2 && index1 != index3 && index2 != index3)
+                {
+                    winning = 0;
+
+                    // Display winnings
+                    MessageBox.Show("You won " + winning.ToString("c"));
+                }
+
+                // Clear text box
+                moneyTextBox.Clear();
+                moneyTextBox.Focus();
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid entry.");
+                moneyTextBox.Focus();
+            } // end else
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Display totals
+            MessageBox.Show("You entered " + moneyTotal.ToString("c") + ". You won " + winTotal.ToString("c"));
+
+            // Close the form
+            this.Close();
+
+        }
+    }
+}
